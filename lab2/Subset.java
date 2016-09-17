21df0cac8022a4c2117d739bbfd73b6f02c6318a
colorlight <colorlightleecaa@gmail.com>
2016-09-17 22:05:15 +0800

    lab2:beta:Subset

---
+++ lab2/Subset.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+public class Subset{
+    public static void main(String[] args){
+        String[] inArray;
+        String output;
+        String argument = args[0];
+        int k = Integer.parseInt(argument);
+        RandomizedQueue<String> test = new RandomizedQueue<String>();
+     
+        inArray = StdIn.readAllStrings();
+        for(int i = 0; i<inArray.length; i++)
+            test.enqueue(inArray[i]);
+        StdOut.println("N :" + inArray.length);
+   
+        for(int i = 0; i<k; i++){
+            output = test.dequeue();
+            StdOut.println(output);
+        }
+    }
+}
